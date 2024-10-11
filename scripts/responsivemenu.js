2127db4cb721a7ba444b735410ddacc7b1625709
hello84037 <hello84037@gmail.com>
2024-10-11 08:51:28 -0600

    Fancy Hamburger Menu

--- scripts/responsivemenu.js
+++ scripts/responsivemenu.js
@@ -1,7 +1,18 @@
 const hamburger = document.querySelector(".menu");
 const navigation = document.querySelector(".navigation");
+let showMenu = false;
+
+function toggleMenu() {
+    navigation.classList.toggle("open");
+    if(!showMenu){
+        hamburger.classList.add("close");
+        showMenu = true;
+    } else {
+        hamburger.classList.remove("close");
+        showMenu = false;
+    }
+}
 
 hamburger.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    hamburger.classList.toggle("open");
+    toggleMenu();
 });
