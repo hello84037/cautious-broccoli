c8d7a241b7cb4cf351b0dcfd315633161123261b
hello84037 <hello84037@gmail.com>
2024-10-15 22:18:42 -0600

    Updating the navigation menus.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,25 +1,35 @@
+let addClicked = false;
+let removeClicked = false;
+
 addButton.addEventListener(
     "click",
     function () {
         myPopup.classList.add("show");
+        // The Add button was clicked.
+    }
+);
+
+removeButton.addEventListener(
+    "click",
+    function () {
+        myPopup.classList.add("show");
+        // The Remove button was clicked.
     }
 );
 
 barcodeLookup.addEventListener(
     "click",
     function () {
-        myPopup.classList.remove(
-            "show"
-        );
+        myPopup.classList.remove("show");
+        // barcode lookup was clicked
     }
 );
 
 manualLookup.addEventListener(
     "click",
     function () {
-        myPopup.classList.remove(
-            "show"
-        );
+        myPopup.classList.remove("show");
+        // manual lookup was clicked
     }
 );
 
